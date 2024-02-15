29322ba846fd02eefccce760f264dbf98be1a391
Ayush Ranjan <ranaayush0725@gmail.com>
2024-02-16 02:48:22 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const { get } = require('http');
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorhandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+//we use .use for middleware
+
+/////////////MIDDLEWARES////////////////
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+/////////ROUTES////////////
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't Find ${req.originalUrl} on this server`,
+  // });
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorhandler);
+
+module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This is not buld yet :(',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This is not buld yet :(',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This is not buld yet :(',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This is not buld yet :(',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This is not buld yet :(',
+  });
+};
